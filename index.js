03a857ffb2a29689b5cd575746966dcdd4fc5d38
relational-kernel-3847 <hoolio.osuna@gmail.com>
2020-08-02 20:04:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(hello){
   if (hello === 'hello'){
     return 'I can\'t hear you!'
   } else if (hello === 'HELLO') {
-    returns 'YES INDEED!'
+    return 'YES INDEED!'
   } else if (hello === 'I love you, Grandma.'){
     return 'I love you, too.'
   }
